eeaabd0dbbc422f2a5e0b6d5830b47343ddf7d8e
largoudigital-dot <largou.digital@gmail.com>
2026-05-03 18:26:25 +0200

    Initial Commit

---
+++ PureColor/PureColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureColorApp.swift
+//  PureColor
+//
+//  Created by Largou on 03.05.26.
+//
+
+import SwiftUI
+
+@main
+struct PureColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PureColorTests/PureColorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PureColorTests.swift
+//  PureColorTests
+//
+//  Created by Largou on 03.05.26.
+//
+
+import Testing
+@testable import PureColor
+
+struct PureColorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
